757e10311d058fcb18ed19f67f5b3b52e6755844
Ogulcan Kendirci <93669966+ken0gul@users.noreply.github.com>
2023-06-18 14:01:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const { exec, spawn } = require("child_process");
 const cors = require("cors");
 
 const app = express();
-// app.use(cors());
+
 const corsOptions = {
   origin: "https://coding-arena-production.up.railway.app", // Replace with the allowed origin for the specific request
+  methods: "POST", // Specify the allowed HTTP methods
 };
 
 app.use(express.json());
@@ -21,47 +22,9 @@ const tempDirectory = path.join(
 fs.mkdirSync(tempDirectory, { recursive: true });
 
 app.post("/execute", cors(corsOptions), (req, res) => {
-  const javaCode = req.body.code;
-  const filePath = path.join(tempDirectory, "Code.java");
-  console.log(filePath);
+  // Your existing code for executing Java code
 
-  // Create a temporary Java file and write the code to it
-  fs.writeFileSync(filePath, javaCode);
-
-  // Compile and execute the Java code
-  exec(`javac ${filePath} && java ${filePath}`, (error, stdout, stderr) => {
-    if (error) {
-      res.status(500).json({ error: error.message });
-      return;
-    }
-    if (stderr) {
-      res.status(500).json({ error: stderr });
-      return;
-    }
-    console.log(stdout);
-    let result = stdout === "supmaCsredoC";
-
-    res
-      .status(200)
-      .json({ output: result ? `Congrats! => ${stdout}` : "Try Again!" });
-  });
-
-  const javaProcess = spawn("java", [filePath]);
-
-  javaProcess.stdout.on("data", (data) => {
-    const output = data.toString().trim();
-    // Process the output here if needed
-    console.log(`Java code output: ${output}`);
-  });
-
-  javaProcess.stderr.on("data", (data) => {
-    console.error(`Java code error: ${data}`);
-  });
-
-  javaProcess.on("close", (code) => {
-    // `code` contains the exit code of the Java process
-    console.log(`Java code exited with code ${code}`);
-  });
+  // Rest of your code
 });
 
 const port = 3000;
