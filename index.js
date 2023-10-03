bdd0e79851e4671e3095dae792c5a2f7517ebcd4
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-10-03 14:24:41 -0400

    updated stdout text

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ console.log("asdasd");
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join("./", "public")));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://coding-arena-production.up.railway.app"); // Replace "*" with the specific origin you want to allow, or use "*" to allow any origin
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://coding-arena-production.up.railway.app"
+  ); // Replace "*" with the specific origin you want to allow, or use "*" to allow any origin
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify the HTTP methods allowed by your application
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Specify the headers allowed in the request
-  res.header("Access-Control-Max-Age" ,"3600");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); // Specify the headers allowed in the request
+  res.header("Access-Control-Max-Age", "3600");
   next();
 });
 const server = http.createServer(app);
@@ -29,14 +35,19 @@ const tempDirectory = path.join("/", tempDirectoryName);
 fs.mkdirSync(tempDirectory, { recursive: true });
 
 app.post("/execute", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://coding-arena-production.up.railway.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify the HTTP methods allowed by your application
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); // Specify the headers allowed in the request
-  res.header("Access-Control-Max-Age" ,"3600");
- 
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://coding-arena-production.up.railway.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify the HTTP methods allowed by your application
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); // Specify the headers allowed in the request
+  res.header("Access-Control-Max-Age", "3600");
+
   const javaCode = req.body.code;
   const filePath = path.join(tempDirectory, "Code.java");
- 
 
   // Create a temporary Java file and write the code to it
   fs.writeFileSync(filePath, javaCode);
@@ -51,17 +62,14 @@ app.post("/execute", (req, res) => {
       res.status(500).json({ error: stderr });
       return;
     }
-    console.log(stdout);
-    let result = stdout === "supmaCsredoC";
+    // codingArena
+    let result = stdout === "anerAgnidoc";
 
     res
       .status(200)
       .json({ output: result ? `Congrats! => ${stdout}` : "Try Again!" });
   });
 });
-
-
-
 
 server.listen(3001, () => {
   console.log(`Server running on http://localhost:3001`);
